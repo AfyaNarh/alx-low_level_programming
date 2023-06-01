cb7edd97ee9c37965a67847075dd06d4c1e43552
AfyaNarh <nhyiraafya1@gmail.com>
2023-06-01 15:10:00 -0700

    Update

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,4 +1,3 @@
-
 #ifndef LISTS_H
 #define LISTS_H
 
@@ -14,11 +13,12 @@
  *
  * Description: singly linked list node structure
  */
+
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 int _putchar(char c);
